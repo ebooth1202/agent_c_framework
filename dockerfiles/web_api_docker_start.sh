df0df0546e3aa05e4bdbe394fab61cecdf3fd58f
justjoehere <joseph.ours@gmail.com>
2025-03-05 14:24:37 -0500

    Creating useful name for those using the start.bat/start.sh

--- dockerfiles/web_api_docker_start.sh
+++ dockerfiles/web_api_docker_start.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 # Build and start the containers, uncomment to run in attached mode:
-#docker-compose up --build
+#docker-compose -p agent_c_api_reactui up --build
 
 # To run in detached mode (background), uncomment to run in detached mode:
- docker-compose up --build -d
+ docker-compose -p agent_c_api_reactui up --build -d
