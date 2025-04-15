dd2611d534630d2a8325d4c57b49094ef16a1438
justjoehere <joseph.ours@gmail.com>
2025-04-15 14:04:12 -0400

    Fixing RAG startup

--- scripts/start_fe.bat
+++ scripts/start_fe.bat
@@ -1,6 +1,6 @@
 @echo off
 SETLOCAL
 SET VITE_API_URL=http://localhost:8000/api/v1
-SET VITE_RAG_API_URL=http://localhost:8000/api/v1
+SET VITE_RAG_API_URL=http://localhost:8001/api/v1
 cd src\agent_c_api_ui\agent_c_react_client
 npm run dev
