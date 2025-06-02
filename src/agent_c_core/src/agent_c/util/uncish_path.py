f5a549ede839fe84a1842caaafa547fa4cab60c8
Donavan <donavan.stanley@gmail.com>
2025-06-02 10:19:54 -0400

    UNCPath fixes

--- src/agent_c_core/src/agent_c/util/uncish_path.py
+++ src/agent_c_core/src/agent_c/util/uncish_path.py
@@ -6,9 +6,10 @@ class UNCishPath:
     def __init__(self, path: str):
         if not path.startswith("//"):
             raise ValueError(f"Invalid path format: {path}. Must start with //")
-        source, path = path.split('/', 1)
 
-        self.source = source[2:]
+        source, path = path.removeprefix("//").split('/', 1)
+
+        self.source = source
         self.path = path if path else "./"
         self.path_parts = self.path.split("/")
 
