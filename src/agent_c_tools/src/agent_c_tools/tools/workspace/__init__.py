4f7bb356efac609a95cfba117a8613c0a38ef2ae
justjoehere <joseph.ours@gmail.com>
2025-08-28 13:35:47 -0400

    Addign dynamictoolset import to folder

--- src/agent_c_tools/src/agent_c_tools/tools/workspace/__init__.py
+++ src/agent_c_tools/src/agent_c_tools/tools/workspace/__init__.py
@@ -2,3 +2,4 @@ from .tool import WorkspaceTools
 from .prompt import WorkspaceSection
 from .local_storage import LocalStorageWorkspace
 from .s3_storage import S3StorageWorkspace
+from .dynamic_command import DynamicCommandToolset
