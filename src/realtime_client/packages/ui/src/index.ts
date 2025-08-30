2bab317a35b6f9a56163a7f4d3920415877cd7ec
Donavan <donavan.stanley@gmail.com>
2025-08-30 13:37:24 -0400

    Prepping for ui components package

---
+++ src/realtime_client/packages/ui/src/index.ts
@@ -0,0 +1,7 @@
+// Utility exports
+export { cn } from './lib/utils';
+
+// Component exports will be added here as we build them
+// export { ConnectionButton } from './components/ConnectionButton';
+// export { MicrophoneButton } from './components/MicrophoneButton';
+// etc...
