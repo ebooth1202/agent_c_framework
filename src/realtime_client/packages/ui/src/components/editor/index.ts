5f420a660e4b61a7041d7514065c27401a825b6b
Donavan <donavan.stanley@gmail.com>
2025-09-17 12:03:33 -0400

    Remove unused exports from index.ts to streamline module interface

--- src/realtime_client/packages/ui/src/components/editor/index.ts
+++ src/realtime_client/packages/ui/src/components/editor/index.ts
@@ -5,7 +5,5 @@
 export { MarkdownEditor, keyboardShortcuts } from './MarkdownEditor';
 export { MarkdownEditorClient } from './MarkdownEditorClient';
 export type { MarkdownEditorProps } from './types';
-export { CodeHighlightExample } from './CodeHighlightExample';
-export { SmartPasteExample } from './SmartPasteExample';
 export { SmartPasteExtension } from './SmartPasteExtension';
 export type { SmartPasteOptions, UploadState } from './SmartPasteExtension';
